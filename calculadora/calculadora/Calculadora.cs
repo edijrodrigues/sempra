5dfe19e432dfd30d5ccb3f6223d3b3729a711825
Edi Carlos José Rodrigues <edijrodrigues@gmail.com>
2013-05-03 20:58:33 -0300

    add estrutura da calculadora

---
+++ calculadora/calculadora/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calculadora
+{
+    class Calculadora
+    {
+
+        public Calculadora()
+	    {
+            Console.WriteLine("Objeto inicializado.");
+	    }
+
+
+
+    }
+}
---
+++ calculadora/calculadora/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            
+            
+            
+
+            Console.Read();
+        }
+    }
+}
+
